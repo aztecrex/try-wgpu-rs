61c6e49b75f4a17a24043e825bf12318d03f95df
Greg Wiley <aztec.rex@jammm.com>
2022-06-05 10:01:14 -0700

    basic window

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 use wgpu::{Backends, Instance};
-
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
 fn main() {
     let instances = Instance::new(Backends::all());
     for adapter in instances.enumerate_adapters(Backends::all()) {
         println!("{:?}", adapter.get_info());
     }
+
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("Try wgpu");
+    env_logger::init();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
 }
